a89b1194f92d5ad73b0b8eb961a3f801217c6a2c
rishikesh-panda <rishikeshpanda18@gmail.com>
2026-07-09 12:30:52 +0000

    Conversion of Datatypes Lession

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,1 +1,32 @@
 let score = "33"
+
+console.log(typeof score); // you also write like this 
+console.log(typeof(score)); // and also like this 
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// Conversion Of Datatypes Result -:
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// Conversion Of Datatypes Result -:
+// 1 => true; 0 => false
+// "" => false
+// "Rishi" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
